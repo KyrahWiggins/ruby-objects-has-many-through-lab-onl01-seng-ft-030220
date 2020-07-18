25eb0bb06874407dcaa1fa0aef3f7ad38502818f
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 09:01:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,34 @@
+require 'pry'
 class Artist
-  @@all = []
-  attr_accessor :name
+attr_accessor :name, :genre
 
-  def initialize(name)
-    @name = name
-    self.class.all << self
-  end
 
-  def self.all
-    @@all
-  end
+@@all = []
 
-  def new_song(title, genre)
-    Song.new(title, self, genre)
-  end
+def initialize(name)
+  @name = name
+  @@all << self
+end
 
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
+def self.all
+  @@all
+end
+
+def new_song(name, genre)
+  Song.new(name, self, genre)
+end
+
+def songs
+  #binding.pry
+  Song.all.select do |song|
+    song.artist == self
   end
+end
+
+def genres
+  songs.map do |song|
+    song.genre
+  end
+end
+
+end
