86cbaeb67fc051544a791cf1668ac0cd319945d2
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 08:55:40 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,26 +1,24 @@
 class Patient
-
-  @@all = []
-  attr_accessor :name
+  attr_accessor :name 
+  
   def initialize(name)
-    @name = name
-    @@all << self
+    @name=name
   end
-
-  def self.all
-    @@all
-  end
-
-  def new_appointment(date, doctor)
-    Appointment.new(date, doctor, self)
+  
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
   end
 
   def appointments
-    Appointment.all.select{|apt|apt.patient == self}
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
 
   def doctors
-    appointments.map{|apt|apt.date}
+    appointments.map do |appointment|
+      appointment.doctor
+    end
   end
-
+  
 end
