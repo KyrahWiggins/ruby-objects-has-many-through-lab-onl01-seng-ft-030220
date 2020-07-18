0c0a9e16d752ae3366f9e5b5205da27a79f13890
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-18 08:33:04 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,27 +1,20 @@
+require_relative "./patient"
+require_relative "./doctor"
 
 class Appointment
-
-  attr_reader :patient, :doctor, :date
+  attr_accessor :date, :doctor, :patient
 
   @@all = []
 
-  def initialize(patient ="", doctor = "", date = "")
-    @patient = patient
-    @doctor = doctor
+  def initialize(patient, doctor, date)
     @date = date
+    @doctor = doctor
+    @patient = patient
     @@all << self
   end
 
   def self.all
     @@all
-  end
-
-  def patients
-    self.patient
-  end
-
-  def doctors
-    self.doctor
   end
 
 end
